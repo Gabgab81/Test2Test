36b32e07b4ca96b13dfb4025668a0523b3131411
gabriel roux <rouxgab81@gmail.com>
2023-04-22 10:05:15 -0400

    fixtures users and test model user

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,23 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
+
   # test "the truth" do
   #   assert true
   # end
+
+  def setup
+    @user = users(:uservalid)
+    # login_as users(:uservalid)
+  end
+
+  test 'user should be valid' do
+    assert @user.valid?
+  end
+
+  test "email should be present" do
+    @user.email = ''
+    assert_not @user.valid?
+  end
+
 end
